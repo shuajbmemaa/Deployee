2549c1e081400ec6da90f62d138798c85b411b6f
shuajbkingu <shuajbmemaa@hotmail.com>
2024-12-04 19:47:15 +0100

    Add Priority and Status enums to define task priorities and statuses in the domain layer

---
+++ Deployee.Domain/Enums/Status.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deployee.Domain.Enums;
+
+public enum Status
+{
+    NotStarted = 1,
+    Started = 2,
+    Completed = 3,
+    Overdue = 4
+}
