ee40695172ac57cef84ee12d497a3d362de02ecc
shuajbkingu <shuajbmemaa@hotmail.com>
2024-12-04 19:51:52 +0100

    Add AuthModule for Identity configuration and authentication setup

---
+++ Deployee/Modules/AuthModule.cs
@@ -0,0 +1,23 @@
+﻿using Deployee.Domain.Abstractions;
+using Deployee.Domain.Entities;
+using Deployee.Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace Deployee.Modules;
+
+public class AuthModule : IModule
+{
+    public void Load(IServiceCollection services)
+    {
+        services
+        .AddIdentity<User, IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
+        services.ConfigureApplicationCookie(options =>
+        {
+            options.LoginPath = "/Users/Login";
+        });
+    }
+}
