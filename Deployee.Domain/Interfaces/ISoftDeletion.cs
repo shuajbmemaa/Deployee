27b2cff23ebab0fc58be4480fd8af7427e11a573
shuajbkingu <shuajbmemaa@hotmail.com>
2024-12-04 19:54:56 +0100

    Add ISoftDeletion interface to handle soft delete functionality with undo support

---
+++ Deployee.Domain/Interfaces/ISoftDeletion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deployee.Domain.Interfaces;
+
+public interface ISoftDeletion
+{
+    public bool IsDeleted { get; set; }
+
+    public DateTimeOffset? DeletedAt { get; set; }
+
+    public void Undo()
+    {
+        IsDeleted = false;
+        DeletedAt = null;
+    }
+}
