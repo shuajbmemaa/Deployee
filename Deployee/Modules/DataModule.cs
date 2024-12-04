4f882c7f299963b98245f64ce1427491f79fbe3b
shuajbkingu <shuajbmemaa@hotmail.com>
2024-12-04 19:51:28 +0100

    Implement DataModule for configuring database context and SQL Server connection

---
+++ Deployee/Modules/DataModule.cs
@@ -0,0 +1,34 @@
+﻿using Deployee.Domain.Abstractions;
+using Deployee.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Configuration;
+
+namespace Deployee.Modules;
+
+public class DataModule : IModule
+{
+    private readonly IConfiguration _configuration;
+
+    public DataModule(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public void Load(IServiceCollection services)
+    {
+        var connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+        if (connectionString is null)
+        {
+            throw new ConfigurationErrorsException("Cannot find 'DefaultConnection' section inside the configuration");
+        }
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options
+                .UseSqlServer(connectionString)
+                .AddInterceptors(new SoftDeleteInterceptor())
+        );
+    }
+    }
+
