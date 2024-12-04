6aab2319a009b139aee5b5e676716c79f44b6c95
shuajbkingu <shuajbmemaa@hotmail.com>
2024-12-04 19:54:07 +0100

    Add modular service loading support with IModule interface and AddModule extension

---
+++ Deployee/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Deployee.Domain.Abstractions;
+
+namespace Deployee.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddModule(this IServiceCollection services, IModule module)
+    {
+        if (services is null) throw new ArgumentNullException(nameof(services));
+        if (module is null) throw new ArgumentNullException(nameof(module));
+
+        module.Load(services);
+        return services;
+    }
+}
