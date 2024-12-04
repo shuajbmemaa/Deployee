1d5c9f1ad2f35a735e3af2b13b92b4cf26292978
shuajbkingu <shuajbmemaa@hotmail.com>
2024-12-04 19:48:50 +0100

    Add validators for user registration and login forms using FluentValidation

---
+++ Deployee/Validators/LoginModelValidator.cs
@@ -0,0 +1,20 @@
+﻿using Deployee.Models;
+using FluentValidation;
+
+namespace Deployee.Validators;
+
+public class LoginModelValidator : AbstractValidator<LoginViewModel>
+{
+    public LoginModelValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("Email is required")
+            .EmailAddress().WithMessage("A valid email address is required.");
+
+        RuleFor(x => x.Password)
+            .NotNull()
+            .NotEmpty().WithMessage("Password is required.");
+    }
+}
---
+++ Deployee/Validators/RegisterModelValidator.cs
@@ -0,0 +1,34 @@
+﻿using Deployee.Models;
+using FluentValidation;
+
+namespace Deployee.Web.Validators;
+
+public class RegisterViewModelValidator : AbstractValidator<RegisterViewModel>
+{
+    public RegisterViewModelValidator()
+    {
+        RuleFor(x => x.Email)
+           .NotEmpty().WithMessage("Email is required.")
+           .EmailAddress().WithMessage("A valid email address is required.");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required.")
+            .MinimumLength(6).WithMessage("Password must be at least 6 characters long.")
+            .Matches("[A-Z]").WithMessage("Password must contain at least one uppercase letter.")
+            .Matches("[a-z]").WithMessage("Password must contain at least one lowercase letter.")
+            .Matches("[0-9]").WithMessage("Password must contain at least one number.")
+            .Matches("[^a-zA-Z0-9]").WithMessage("Password must contain at least one special character.");
+
+        RuleFor(x => x.ConfirmPassword)
+            .NotEmpty().WithMessage("Confirmation password is required.")
+            .Equal(x => x.Password).WithMessage("Password and confirmation password must match.");
+
+        RuleFor(x => x.FirstName)
+           .NotEmpty().WithMessage("First name is required.")
+           .Matches("^[a-zA-Z]+$").WithMessage("First name must contain only letters.");
+
+        RuleFor(x => x.LastName)
+            .NotEmpty().WithMessage("Last name is required.")
+            .Matches("^[a-zA-Z]+$").WithMessage("Last name must contain only letters.");
+    }
+}
