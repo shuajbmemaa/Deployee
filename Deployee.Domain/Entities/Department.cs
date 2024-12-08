83b0c2921ba5353c17f25be904a9759191f83820
shuajbkingu <shuajbmemaa@hotmail.com>
2024-12-08 19:33:39 +0100

    Add Soft Deletion to Entities

--- Deployee.Domain/Entities/Department.cs
+++ Deployee.Domain/Entities/Department.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Deployee.Domain.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Deployee.Domain.Entities;
 
-public class Department
+public class Department : IEntity<int>
 {
     [Key]
     public int Id { get; set; }
